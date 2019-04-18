2519a45b050c91a26c0c7aa2dbb33b1f5040a95d
heming6666 <1040023934@qq.com>
2019-04-18 13:04:25 +0800

    add Congratulations modal

--- src/Scenario.js
+++ src/Scenario.js
@@ -4,103 +4,114 @@ import SplitPane from "react-split-pane";
 import {resolvePath} from "mobx-state-tree";
 import Step from "./Step";
 import Term from "./Term";
-import {Button, Icon, notification} from "antd";
+import {Button, Icon, notification,Modal} from "antd";
 import {Link} from "react-router-dom";
 
-class Scenario extends Component {
-  state = {
-    stepIndex: 0
-  }
-
-  componentWillUpdate() {
-    if (this.props.store.course.scenarios[this.props.match.params.index]) {
-      this.props.store.course.scenarios[this.props.match.params.index].createContainer();
-    }
-  }
-  componentDidMount() {
-    if (this.props.store.course.scenarios[this.props.match.params.index]) {
-      this.props.store.course.scenarios[this.props.match.params.index].createContainer();
-    }
-  }
-
-  componentWillUnmount() {
-    this.props.store.course.scenarios[this.props.match.params.index].clearContainer();
-  }
-
-  setComplete() {
-    if (this.props.match.params.index == this.props.store.completeIndex) {
-      this.props.store.setCompleteIndex(this.props.match.params.index * 1 + 1);
-    }
-  }
-
-  openNotification() {
-    notification['info']({
-      message: '提示',
-      description: '您还未完成本步骤的内容！',
+function showModal() {
+    Modal.success({
+        title: 'Congratulations!',
+        content: '恭喜你！你已完成全部 Git 实训课程！',
     });
-  }
+}
 
-  render() {
-    let scenario = this.props.store.course.scenarios[this.props.match.params.index];
-    if (!scenario) {
-      return <div></div>
+class Scenario extends Component {
+    state = {
+        stepIndex: 0
     }
 
-    return (
-      <SplitPane split="vertical" minSize={50} defaultSize={400} style={{position: 'relative'}}>
-        <div style={{height: '100%', overflow: 'auto'}}>
-          <div style={{
-            height: 40,
-            lineHeight: '40px',
-            textAlign: 'center',
-            fontSize: 24,
-            background: '#3095d2',
-            color: '#fff'
-          }}>{scenario.title}</div>
-          <Step step={scenario.steps[this.state.stepIndex]} scenario={scenario}/>
-          <div style={{padding: 20}}>
-            {this.state.stepIndex != 0 &&
-            <Button type="default" onClick={() => {
-              this.setState({stepIndex: this.state.stepIndex - 1})
-            }}>
-              <Icon type="left"/>上一步
-            </Button>
-            }
-            {this.state.stepIndex != scenario.steps.length - 1 &&
-            <Button type="primary" style={{float: 'right'}} onClick={() => {
-              scenario.steps[this.state.stepIndex].checkstep().then(d => {
-                if (d == true) {
-                  this.setState({stepIndex: this.state.stepIndex + 1})
-                } else {
-                  this.openNotification();
-                }
-              });
-            }}>
-              下一步<Icon type="right"/>
-            </Button>
-            }
-            {this.state.stepIndex == scenario.steps.length - 1 &&
-            <Button type="primary" style={{float: 'right'}} onClick={() => {
-              scenario.steps[this.state.stepIndex].checkstep().then(d => {
-                if (d == true) {
-                  this.props.history.push('/' + window.location.hash);
-                  this.setComplete();
-                } else {
-                  this.openNotification();
-                }
-              });
-            }}>
-              返回目录<Icon type="book"/>
-            </Button>
-            }
-          </div>
-        </div>
-        <div style={{height: '100%', background: '#000', overflow: 'hidden'}}>
-          <Term scenario={scenario} style={{height: '100%'}}/>
-        </div>
-      </SplitPane>
-    )
-  }
+    componentWillUpdate() {
+        if (this.props.store.course.scenarios[this.props.match.params.index]) {
+            this.props.store.course.scenarios[this.props.match.params.index].createContainer();
+        }
+    }
+    componentDidMount() {
+        if (this.props.store.course.scenarios[this.props.match.params.index]) {
+            this.props.store.course.scenarios[this.props.match.params.index].createContainer();
+        }
+    }
+
+    componentWillUnmount() {
+        this.props.store.course.scenarios[this.props.match.params.index].clearContainer();
+    }
+
+    setComplete() {
+        if (this.props.match.params.index == this.props.store.completeIndex) {
+            this.props.store.setCompleteIndex(this.props.match.params.index * 1 + 1);
+        }
+    }
+
+    openNotification() {
+        notification['info']({
+            message: '提示',
+            description: '您还未完成本步骤的内容！',
+        });
+    }
+
+    render() {
+        let scenario = this.props.store.course.scenarios[this.props.match.params.index];
+        if (!scenario) {
+            return <div></div>
+        }
+
+        return (
+            <SplitPane split="vertical" minSize={50} defaultSize={400} style={{position: 'relative'}}>
+                <div style={{height: '100%', overflow: 'auto'}}>
+                    <div style={{
+                        height: 40,
+                        lineHeight: '40px',
+                        textAlign: 'center',
+                        fontSize: 24,
+                        background: '#3095d2',
+                        color: '#fff'
+                    }}>{scenario.title}</div>
+                    <Step step={scenario.steps[this.state.stepIndex]} scenario={scenario}/>
+                    <div style={{padding: 20}}>
+                        {this.state.stepIndex != 0 &&
+                        <Button type="default" onClick={() => {
+                            this.setState({stepIndex: this.state.stepIndex - 1})
+                        }}>
+                            <Icon type="left"/>上一步
+                        </Button>
+                        }
+                        {this.state.stepIndex != scenario.steps.length - 1 &&
+                        <Button type="primary" style={{float: 'right'}} onClick={() => {
+                            scenario.steps[this.state.stepIndex].checkstep().then(d => {
+                                if (d == true) {
+                                    this.setState({stepIndex: this.state.stepIndex + 1})
+                                } else {
+                                    this.openNotification();
+                                }
+                            });
+                        }}>
+                            下一步<Icon type="right"/>
+                        </Button>
+                        }
+                        {this.state.stepIndex == scenario.steps.length - 1 &&
+                        <Button type="primary" style={{float: 'right'}} onClick={() => {
+                            scenario.steps[this.state.stepIndex].checkstep().then(d => {
+                                if (d == true) {
+                                    this.props.history.push('/' + window.location.hash);
+                                    this.setComplete();
+                                } else {
+                                    this.openNotification();
+                                }
+                            });
+                        }}>
+                            返回目录<Icon type="book"/>
+                        </Button>
+                        }
+                        {this.state.stepIndex == scenario.steps.length - 1 &&
+                        this.props.match.params.index == this.props.store.course.scenarios.length-1 &&
+                        showModal()
+                        }
+                    </div>
+                </div>
+                <div style={{height: '100%', background: '#000', overflow: 'hidden'}}>
+                    <Term scenario={scenario} style={{height: '100%'}}/>
+                </div>
+            </SplitPane>
+        )
+    }
 }
 
 export default inject('store')(observer(Scenario));
