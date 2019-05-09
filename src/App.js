a60ee71e6977c714ca74a207970abe66ea5947bc
guodong <gd@tongji.edu.cn>
2019-05-09 13:18:49 +0800

    top nav

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import {inject, observer} from "mobx-react";
 import {Route, BrowserRouter as Router} from "react-router-dom";
 import Scenario from "./Scenario";
 import LoadingPage from "./LoadingPage";
-
+import {Icon, Layout, Menu} from "antd";
+const { Header, Content, Footer } = Layout;
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="wrapper">
-          <div style={{background: '#345d86'}}>
+          <Header style={{padding: 0}}>
             <div style={{fontSize: 24, color: '#fff', float: 'left'}}><img
               src='http://kfcoding.com/static/logo-min.d61eb61d.png' style={{height: 64}}/> GitCourse
             </div>
-          </div>
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              style={{lineHeight: '64px', float: 'right'}}
+            >
+              <Menu.Item key="1"><a href='http://kfcoding.com'><Icon type="bank" />KFCODING</a></Menu.Item>
+              <Menu.Item key="2"><a href='https://github.com/guodong/gitcourse' target='_blank'><Icon type="github" />GITHUB</a></Menu.Item>
+            </Menu>
+          </Header>
           <Route exact path="/" component={Course}/>
           <Route path="/scenarios/:index" component={Scenario}/>
           {this.props.store.loading && <LoadingPage/>}
