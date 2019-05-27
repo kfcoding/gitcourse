2ed73f63136b2a548508837a8d9dc9f32f2ba68d
guodong <gd@tongji.edu.cn>
2019-05-28 01:40:23 +0800

    image local

--- src/Step.js
+++ src/Step.js
@@ -3,8 +3,33 @@ import ReactMarkdown from 'react-markdown';
 import CodeBlock from './CodeBlock';
 import {inject, observer} from 'mobx-react';
 
-function Image(props) {
-  return <img {...props} style={{maxWidth: '100%'}} />
+// function Image(props) {console.log(props)
+//   if (props.src.substr(0, 4) == 'http') {
+//     return <img {...props} style={{maxWidth: '100%'}}/>
+//   } else {
+//
+//   }
+// }
+
+class Image extends Component {
+  state = {
+    src: this.props.src,
+    dsrc: '',
+  }
+
+  componentDidMount() {
+    if (this.state.src.substr(0, 4) == 'http') {
+      this.setState({dsrc: this.state.src});
+    } else {
+      this.props.store.pfs.readFile(this.props.store.dir + this.props.src).then(d => this.setState({dsrc: window.URL.createObjectURL(new Blob([d]))}));
+    }
+  }
+
+  render() {
+    return (
+      <img src={this.state.dsrc} style={{maxWidth: '100%'}}/>
+    );
+  }
 }
 
 class Step extends Component {
@@ -21,10 +46,14 @@ class Step extends Component {
     this.props.step.preloadstep();
   }
 
+  renderImage(props) {
+    return <Image store={this.props.store} src={props.src}/>
+  }
+
   render() {
     return (
       <div style={{padding: 20, overflow: 'auto'}}>
-        <ReactMarkdown source={this.props.step.content} renderers={{inlineCode: CodeBlock, code: CodeBlock, image: Image}} linkTarget='_blank'/>
+        <ReactMarkdown source={this.props.step.content} renderers={{inlineCode: CodeBlock, code: CodeBlock, image: this.renderImage.bind(this)}} linkTarget='_blank'/>
       </div>
     )
   }
